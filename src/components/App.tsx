6b21e9d779df3bbc01c1d24554ff6bfb7282ede8
Adam <adampugh@zoho.com>
2021-03-16 21:52:12 +0000

    added loading

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,21 +3,26 @@ import { useState } from 'react';
 import * as types from 'styled-components/cssprop';
 import * as S from './App.styled';
 
-// import Loading from '../components/Loading/Loading';
+import Loading from '../components/Loading/Loading';
 import TopBanner from '../components/TopBanner/TopBanner';
 import RainCloud from '../components/RainCloud/RainCloud';
 import AudioPlayer from '../components/AudioPlayer/AudioPlayer';
 
 const App: React.FC = () => {
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [audio, setAudio] = useState('');
 
     return (
         <S.App>
-            {/* { loading ? <Loading /> : <div></div> } */}
-            <TopBanner />
-            <RainCloud />
-            <AudioPlayer />
+            {loading ? (
+                <Loading />
+            ) : (
+                <>
+                    <TopBanner />
+                    <RainCloud />
+                    <AudioPlayer />
+                </>
+            )}
         </S.App>
     );
 };
