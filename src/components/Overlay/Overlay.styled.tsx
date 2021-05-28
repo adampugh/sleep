5d5a2863becf1638285c7c649645fa6b6883cc3e
Adam <adampugh@zoho.com>
2021-05-28 10:42:57 +0100

    add fade in for overlay

--- src/components/Overlay/Overlay.styled.tsx
+++ src/components/Overlay/Overlay.styled.tsx
@@ -5,7 +5,45 @@ export const Overlay = styled.div`
         position: absolute;
         height: 100vh;
         width: 100vw;
-        background: rgba(0, 0, 0, 0.3);
+        background: rgba(0, 0, 0, 0.8);
         top: 0;
+        -webkit-animation: fadein 0.3s linear forwards;
+        animation: fadein 0.3s linear forwards;
+    }
+
+    @-webkit-keyframes fadein {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+
+    @keyframes fadein {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+
+    @-webkit-keyframes fadeout {
+        0% {
+            opacity: 1;
+        }
+        100% {
+            opacity: 0;
+        }
+    }
+
+    @keyframes fadeout {
+        0% {
+            opacity: 1;
+        }
+        100% {
+            opacity: 0;
+        }
     }
 `;
